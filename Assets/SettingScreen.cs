f1b8acb09d23bad1c12913172d0b47ee6ac0a121
Anna Poglitsch <114026776+annapoglitsch@users.noreply.github.com>
2024-07-03 22:42:55 +0200

    SetActive Of SettingsScreen handled
    If User enters name -< SettingsScreen disabled and MainMenu enabled

--- Assets/SettingScreen.cs
+++ Assets/SettingScreen.cs
@@ -1,23 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SettingScreen : MonoBehaviour { 
     private string input;
-    //public Player _player;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void ReadInput(string username)
     {
         input = username;
